38bc13c06864089fc592fdf075a6b5a94f1b8703
Bhagyaashreee <shreebhagyaaa@gmail.com>
2026-07-23 19:37:57 +0530

    Implement fully responsive fluid UI overhaul

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <div>&copy; {new Date().getFullYear()} Bhagyashree Satpathy. All Rights Reserved.</div>
-      <div>Designed with &hearts; for the modern web.</div>
+      <div>Designed &amp; Developed by BS</div>
     </footer>
   );
 }
